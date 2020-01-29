f0703bee326b28ed6752174ffe124204ea42934a
Ben Whitney <ben.whitn@gmail.com>
2020-01-29 20:16:10 +0000

    asdsd

---
+++ index.js
@@ -0,0 +1,48 @@
+$(".question").click(function() {
+  if (
+    $(this)
+      .next(".answer")
+      .is(":hidden")
+  ) {
+    $(this)
+      .find("img")
+      .fadeOut("fast", function() {
+        $(this)
+          .attr("src", "./images/icon-arrow-close.svg")
+          .fadeIn("fast");
+      });
+
+    $(this)
+      .next(".answer")
+      .slideDown("slow");
+  } else {
+    $(this)
+      .find("img")
+      .fadeOut("fast", function() {
+        $(this)
+          .attr("src", "./images/icon-arrow.svg")
+          .fadeIn("fast");
+      });
+
+    $(this)
+      .next(".answer")
+      .slideUp("slow");
+  }
+});
+
+$(".tab:first").css("display", "flex");
+
+$(".features-container-tabs a").click(function() {
+  event.preventDefault();
+
+  var tab_id = $(this).attr("href");
+
+  $(".features-container-tabs a").removeClass("fade");
+  $(".tab").hide();
+  $(this).addClass("fade");
+
+  $(tab_id)
+    .css("display", "flex")
+    .hide()
+    .fadeIn("slow");
+});
